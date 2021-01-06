5cf6b050a4d8a4af2a4c48e04d6502d1fa3d0dc7
Sheetal Pathak <sheetal.pathak@fantailtech.com>
2021-01-07 01:21:56 +0530

    mymodule commit

---
+++ README.txt
@@ -0,0 +1,20 @@
+Created Custom module called it mymodule
+
+1. A new form text field named "Site API Key"  adds to the "Basic Site Information" form with 
+the default value of “No API Key yet”.
+
+2.When this form is submitted, the value that the user entered for this field is saved as the system variable 
+named "siteapikey".
+
+3.A Drupal message informs the user that the Site API Key has been saved with that value.
+
+
+4. When this form is visited after the "Site API Key" is saved, the is populated with the correct value.
+
+
+5. The text of the "Save configuration" button  changes to "Update Configuration".
+
+
+Links Refered
+1. Refered drupal.org and stackoverflow.com
+2. Spended 3.5 hours to complete these tasks
---
+++ src/Form/ExtendedSiteInformationForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\mymodule\Form;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\system\Form\SiteInformationForm;
+
+
+class ExtendedSiteInformationForm extends SiteInformationForm {
+ 
+   /**
+   * {@inheritdoc}
+   */
+	  public function buildForm(array $form, FormStateInterface $form_state) {
+		$site_config = $this->config('system.site');
+		$form =  parent::buildForm($form, $form_state);
+		$form['site_information']['siteapikey'] = [
+			'#type' => 'textfield',
+			'#title' => t('Site API Key'),
+			'#default_value' => $site_config->get('siteapikey') ?: 'No API Key yet',
+			'#description' => t("Custom field to set the API Key"),
+		];
+        $form['actions']['submit']['#value'] = t('Update Configuration');
+		
+		return $form;
+	}
+	
+	  public function submitForm(array &$form, FormStateInterface $form_state) {
+		$this->config('system.site')
+		  ->set('siteapikey', $form_state->getValue('siteapikey'))
+          ->save();
+          drupal_set_message($form['site_information']['siteapikey']['#value'].' - Site API Key updated.');
+		parent::submitForm($form, $form_state);
+        
+	  }
+}
